01711fe3e6bf9f006cb9abe6234f58ea3d8c4856
Hai Quoc Nguyen <49754272+thunderhead021@users.noreply.github.com>
2025-01-29 16:12:13 +0100

    wall

--- 2dPriateJam/Assets/Script/Tiles/WallRandomizer.cs
+++ 2dPriateJam/Assets/Script/Tiles/WallRandomizer.cs
@@ -5,11 +5,20 @@ public class WallRandomizer : MonoBehaviour
 {
     public List<Sprite> sprites;
     public SpriteRenderer spriteRenderer;
+    public bool sideWall = false;
     // Start is called before the first frame update
     void Start()
     {
+        int index = 0;
         int rand = Random.Range(0, 100);
-        int index = rand <= 90 ? 0 : rand <= 95 ? 1 : rand <= 99 ? 2 : 3;
+        if (sideWall)
+        {
+            index = rand <= 70 ? 0 : rand <= 90 ? 1 : 2;
+        }
+        else 
+        {
+            index = rand <= 45 ? 0 : rand <= 90 ? 1 : rand <= 95 ? 2 : 3;
+        }
         spriteRenderer.sprite = sprites[index];
     }
 }
